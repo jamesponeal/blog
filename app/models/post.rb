ad3ecc968c6d408c666ff0b3c2b14a5270e789a1
James O'Neal <jamesponeal@gmail.com>
2016-03-27 19:23:59 -0500

    adds associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :author, class_name: :user
+  has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+
 end
--- db/migrate/20160327161010_create_comments.rb
+++ db/migrate/20160327161010_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
-      t.integer :user_id
+      t.integer :author_id
       t.integer :post_id
 
       t.timestamps null: false
